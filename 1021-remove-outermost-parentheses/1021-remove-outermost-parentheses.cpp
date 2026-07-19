a3e673cec2d609480bd8adfd042080a2ac803cc7
Amrendra kumar <143737212+Aman4138@users.noreply.github.com>
2026-07-20 01:34:00 +0530

    Time: 27 ms (5.39%), Space: 67.3 MB (5.47%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans = "";
+        int count = 0;
+
+        // for(int i = 0; i < s.length(); i++) {
+        //     if(s[i] == '(') {
+        //         if(count = 0) {
+        //             ans = ans + s[i];
+        //         }
+        //         count++;
+        //     }
+        //     else {
+        //         count--;
+
+        //         if(count = 0) {
+        //             ans = ans + s[i];
+        //         }
+        //     }
+        // }
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == '(') {
+                if(count != 0) {
+                    ans = ans + s[i];
+                }
+                count++;
+            }
+            else {
+                count--;
+
+                if(count != 0) {
+                    ans = ans + s[i];
+                }
+            }
+        }
+        return ans ;
+    }
+};
