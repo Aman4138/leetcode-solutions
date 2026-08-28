4f0bbcc3d4a77b1138553c9151061e845f57ed02
Amrendra kumar <143737212+Aman4138@users.noreply.github.com>
2026-08-29 02:46:48 +0530

    Time: 231 ms (5.49%), Space: 132.3 MB (5.49%) - LeetHub

--- 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -3,101 +3,84 @@ public:
     string lexPalindromicPermutation(string s, string target) {
 
         int n = s.size();
-
-        // Count characters
         vector<int> cnt(26, 0);
 
-        for (char c : s) {
+        for (char c : s)
             cnt[c - 'a']++;
-        }
 
-        // Check whether palindrome is possible
+        // Check palindrome possible or not
         int odd = 0;
-        char middle = 0;
+        char mid = 0;
 
         for (int i = 0; i < 26; i++) {
             if (cnt[i] % 2) {
                 odd++;
-                middle = 'a' + i;
+                mid = 'a' + i;
             }
         }
 
-        if (odd > 1) {
+        if (odd > 1)
             return "";
-        }
 
-        // Characters needed for left half
+        // Make left half
         vector<int> half(26);
 
-        for (int i = 0; i < 26; i++) {
+        for (int i = 0; i < 26; i++)
             half[i] = cnt[i] / 2;
-        }
 
-        int halfLen = n / 2;
+        int m = n / 2;
         string left = "";
 
-        // Build left half
-        for (int pos = 0; pos < halfLen; pos++) {
+        // Try to make smallest possible answer
+        for (int pos = 0; pos < m; pos++) {
 
-            bool found = false;
+            bool ok = false;
 
-            // Try smallest character first
             for (int c = 0; c < 26; c++) {
 
                 if (half[c] == 0)
                     continue;
 
-                // Take this character
                 left += char('a' + c);
                 half[c]--;
 
-                // Make the biggest possible remaining half
+                // Make the largest possible palindrome
                 string temp = left;
 
                 for (int x = 25; x >= 0; x--) {
-                    temp += string(half[x], char('a' + x));
+                    for (int k = 0; k < half[x]; k++)
+                        temp += char('a' + x);
                 }
 
-                // Make palindrome
                 string pal = temp;
 
-                if (n % 2 == 1) {
-                    pal += middle;
-                }
+                if (n % 2)
+                    pal += mid;
 
-                // Add reverse of left half
-                for (int i = halfLen - 1; i >= 0; i--) {
+                for (int i = m - 1; i >= 0; i--)
                     pal += temp[i];
-                }
 
-                // If it is greater than target,
-                // this character is possible
                 if (pal > target) {
-                    found = true;
+                    ok = true;
                     break;
                 }
 
-                // Otherwise undo
                 left.pop_back();
                 half[c]++;
             }
 
-            // No character can make answer greater
-            if (!found) {
+            if (!ok)
                 return "";
-            }
         }
 
-        // Create final answer
+        // Build answer
         string ans = left;
 
-        if (n % 2 == 1) {
-            ans += middle;
-        }
+        if (n % 2)
+            ans += mid;
 
-        for (int i = halfLen - 1; i >= 0; i--) {
+        for (int i = m - 1; i >= 0; i--)
             ans += left[i];
-        }
 
         return ans > target ? ans : "";
     }
